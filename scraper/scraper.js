5aec4becde4e77056e16229d684f9a678661bec3
wwsalmon <wwsamson@yahoo.com>
2020-03-28 17:26:01 -0400

    add note about current approach

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -10,6 +10,10 @@ axios.get(url).then(res => {
     const markup = $('textarea#wpTextbox1').text();
     const doc = wtf(markup);
 
+    // This code looks at the "Timeline" section of Liquipedia team pages and scrapes the event descriptions, dates, and reference URLs into nice JSON. Two things complicate this process. First, wtf-wikipedia separates references from other content when parsing the section, so they must be re-paired. Second, event descriptions are stored in separate "content" strings by year, with individual events separated by "*" characters and newlines, which are rendered as a list on Liquipedia. My code right now separately cleans references and makes an array out of the bullet point strings, then processes the two arrays into a final "events" array.
+
+    // A better way to do this would be to first separate the timeline into chunks including both the description and reference. This is easy to do with regex/direct string processing, but I'm not sure how to do it with wtf-wikipedia yet, and I want to use wtf-wikipedia because it makes parsing references into JSON super easy. Definitely something to aim for though, processing events fully and pushing them one at a time rather than in multiple separate loops. Would simplify things like multiple URLs drastically.
+
     const timeline = doc.section('Timeline').json();
 
     const tabs = timeline.templates[0];
