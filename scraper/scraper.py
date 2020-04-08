4db0f90c1f6e561446d991d0197b0d7f3cc1cf81
wwsalmon <wwsamson@yahoo.com>
2020-04-08 18:08:46 -0400

    basic mwparser implementation to get timeline

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,13 @@
 from os import path
 import mwparserfromhell as mw
 import requests
+from pprint import pprint
 
 API_URL = "https://liquipedia.net/rocketleague/api.php"
 
-def get_page_text(playername, force_overwrite):
-    if path.isfile(playername + '.txt'):
-        return "no change"
+def fetch_page_text(name, force_overwrite):
+    if path.isfile("data/" + name + ".txt") and not force_overwrite:
+        print("no change")
     else:
         # this code from GitHub https://github.com/earwig/mwparserfromhell/
         params = {
@@ -15,7 +16,7 @@ def get_page_text(playername, force_overwrite):
             "rvprop": "content",
             "rvslots": "main",
             "rvlimit": 1,
-            "titles": playername,
+            "titles": name,
             "format": "json",
             "formatversion": "2",
         }
@@ -24,10 +25,32 @@ def get_page_text(playername, force_overwrite):
         res = req.json()
         text = res["query"]["pages"][0]["revisions"][0]["content"]
 
-        file = open(playername + ".txt", "w")
-        file.write(text)
-        file.close()
+        with open("data/" + name + ".txt", "w", encoding="utf-8") as file:
+            file.write(text)
 
-        return "file created"
+        print("file created")
 
-print(get_page_text("Turbopolsa", False))
+def parse(name):
+    fetch_page_text(name, False) # check to make sure local copy of data exists
+    with open("data/" + name + ".txt", "r", encoding="utf-8") as file:
+        text = file.read()
+
+    return mw.parse(text)
+
+def main():
+    events = []
+
+    tabs = parse("NRG_Esports").get_sections(matches="Timeline")[0].filter_templates(matches="tabs")[0]
+
+    for param in tabs.params:
+        if param.name[:7] == "content":
+            value = str(param.value).replace("\n", "")
+            this_events = value.split("*")
+            for event in this_events[1:]:
+                # eventually, process the event here and push a nicer thing
+                events.append(event)
+
+    print(events)
+
+if __name__ == "__main__":
+    main()
