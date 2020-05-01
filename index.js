53bcf79ce1eabf0933e7bd27adb0d84b389fbfc9
wwsalmon <wwsamson@yahoo.com>
2020-05-01 17:38:01 -0400

    implement scrolling

--- index.js
+++ index.js
@@ -2,37 +2,55 @@ const height = 400; //temp
 const dateWidth = 3;
 const parseDate = d3.timeParse("%Y-%m-%d");
 
-const svg = d3.select("body")
-    .append("svg")
-    .attr("height",height);
-
-d3.json("/data/events.json").then(data => {
+d3.json("/data/events.json", d3.autoType).then(data => {
     const minDate = d3.min(data, d=>parseDate(d.date));
     const maxDate = d3.max(data, d=>parseDate(d.date));
     const elapsedDays = d3.timeDay.count(minDate,maxDate);
-    const width = elapsedDays * dateWidth;
+
+    let width = document.querySelector("body").offsetWidth;
+
+    const svg = d3.select("body").append("svg")
+        .attr("height",height)
+        .attr("width",width)
+        .attr("id","main-container")
+
+    const zoom = d3.zoom()
+        .scaleExtent([0.5,5])
+        .translateExtent([[0,-Infinity],[width,Infinity]])
+        .on("zoom", zoomed);
 
     const x = d3.scaleTime()
-        .domain([minDate, maxDate])
+        .domain(d3.extent(data, d => parseDate(d.date)))
         .range([0,width])
 
-    svg.append("g")
-        .call(d3.axisBottom(x)
-            .ticks(d3.timeMonth)
-            .tickFormat(d3.timeFormat("%B %Y"))
-            .tickSize(height))
+    const xAxis = (g, x) => g.call(d3.axisBottom(x)
+        .ticks(d3.timeMonth)
+        .tickFormat(d3.timeFormat("%B %Y"))
+        .tickSize(height))
         .selectAll("text")
         .attr("transform", `translate(0 ${-height})`)
         .attr("text-anchor","start")
+
+    const gx = svg.append("g")
+        .call(xAxis, x)
 
     svg.selectAll(".event-point")
         .data(data)
         .enter()
         .append("circle")
+        .attr("class","event-point")
         .attr("cx", d => x(parseDate(d.date)))
         .attr("cy",20)
         .attr("r",5)
         .attr("fill","black")
 
-    svg.attr("width",width)
+    svg.call(zoom)
+
+    function zoomed(){
+        const newx = d3.event.transform.rescaleX(x)
+        gx.call(xAxis, newx)
+
+        svg.selectAll(".event-point")
+            .attr("cx", d => newx(parseDate(d.date)))
+    }
 });
